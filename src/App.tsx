c49afeae6a8042e533dab9a48636ea7335256926
Shamimayesmin <shamimayesmin144@gmail.com>
2023-08-24 13:12:08 +0600

    connect components with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice";
 import { RootState } from "./redux/store";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 function App() {
 	
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const {count} = useSelector((state: RootState) => state.counter)
-
+  const dispatch = useDispatch()
 
 	return (
 		<div>
 			<div className="flex gap-5 ml-6 mt-10">
-				<button className="btn bg-green-400 p-3 rounded-xl">Increment</button>
+				<button onClick={()=> dispatch(increment())} className="btn bg-green-400 p-3 rounded-xl">Increment</button>
+				<button onClick={()=> dispatch(incrementByAmount(5))} className="btn bg-green-400 p-3 rounded-xl">Increment by value</button>
 				<div className="bg-sky-300 p-2 rounded-xl">{count}</div>
-				<button className="btn rounded-xl bg-rose-400 p-3">Decrement</button>
+				<button onClick={() => dispatch(decrement())} className="btn rounded-xl bg-rose-400 p-3">Decrement</button>
 			</div>
 		</div>
 	);
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,16 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import type { PayloadAction } from '@reduxjs/toolkit'
 const initialState = {
-	count: 9,
+	count: 0,
 };
 
 const counterSlice = createSlice({
 	name: "counter",
 	initialState,
 	reducers: {
-		increment: (state) => {state.count = state.count + 1},
-		decrement: (state) => {state.count = state.count - 1},
+		increment: (state) => {
+			state.count = state.count + 1;
+		},
+		decrement: (state) => {
+			state.count = state.count - 1;
+		},
+        incrementByAmount: (state,action:PayloadAction<number>)=>{
+            state.count += action.payload
+        }
 	},
 });
 
-export default counterSlice.reducer
+export const { increment, decrement ,incrementByAmount} = counterSlice.actions;
+export default counterSlice.reducer;
